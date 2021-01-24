725f943e33861756c3fa22de975230467a709c38
Taisei Nakajima <ne300085@senshu-u.jp>
2021-01-25 02:43:04 +0900

    Refactoring

--- root_core.rb
+++ root_core.rb
@@ -49,46 +49,42 @@ class RootCore
     @@tokens[result] || result
   end
 
-  def unget_token
-    @scanner.unscan
-  end
-
   def skip_new_line
     @scanner.scan(/\s*\n+\s*/)
   end
 
-  def parse
-    statements
+  def parse_program
+    parse_statements
   end
 
-  def statements
-    unless s = statement
+  def parse_statements
+    unless s = parse_statement
       raise Exception, 'Statement is not exist.'
     end
 
     result = [:statements, s]
     skip_new_line
     until @scanner.eos?
-      result.append(statement)
+      result.append(parse_statement)
       skip_new_line
     end
     result
   end
 
-  def statement
+  def parse_statement
     if get_token(/let\s+/)
-      assign_statement
+      parse_assign_statement
     elsif get_token(/if\s*/)
-      if_statement
+      parse_if_statement
     elsif get_token(/return\s+/)
-      return_statement
+      parse_return_statement
     else
-      expression_statement
+      parse_expression_statement
     end
   end
 
-  def expression_statement
-    unless e = expression
+  def parse_expression_statement
+    unless e = parse_expression
       raise Exception, 'Expect expression, but not exist.'
     end
 
@@ -97,121 +93,116 @@ class RootCore
     [:expression, e]
   end
 
-  def assign_statement
+  def parse_assign_statement
     unless identifier = get_token(/[a-zA-Z\-_]+/)
       raise Exception, 'Expect identifier, but not exist.'
     end
 
     raise Exception, 'Missing =.' unless get_token(/\s*=\s*/)
 
-    value = expression
+    value = parse_expression
     raise Exception, 'Missing ;.' unless get_token(/;/)
 
     [:assign, identifier, value]
   end
 
-  def return_statement
-    e = expression
+  def parse_return_statement
+    e = parse_expression
     get_token(/;/)
     [:return, e]
   end
 
-  def if_statement
+  def parse_if_statement
     token = get_token(/\(/)
     raise Exception 'Missing (.' if token != :lpar
 
-    condition = expression
+    condition = parse_expression
     token = get_token(/\)/)
     raise Exception 'Missing ).' if token != :rpar
 
-    consequence = block
+    consequence = parse_block
     if get_token(/\s*else\s*/)
-      alternative = block
+      alternative = parse_block
       [:if, condition, consequence, alternative]
     else
       [:if, condition, consequence]
     end
   end
 
-  def expression
-    result = sum
+  def parse_expression
+    result = parse_sum
     token = get_token(/\s*(==|!=|[<>]=?)\s*/)
-    result = [token, result, sum] if token
+    result = [token, result, parse_sum] if token
     result
   end
 
-  def sum
-    result = term
+  def parse_sum
+    result = parse_term
     token = get_token(/\s*[+-]\s*/)
     while (token == :add) || (token == :sub)
-      result = [token, result, term]
+      result = [token, result, parse_term]
       token = get_token(/\s*[+-]\s*/)
     end
     result
   end
 
-  def term
-    result = factor
+  def parse_term
+    result = parse_factor
     token = get_token(%r{\s*[/\*]\s*})
     while (token == :mul) || (token == :div)
-      result = [token, result, factor]
+      result = [token, result, parse_factor]
       token = get_token(%r{\s*[/\*]\s*})
     end
     result
   end
 
-  def factor
+  def parse_factor
     token = get_token(/\d+|\(|[-!]|fn\s*|"|true|false|[a-zA-Z\-_]+/)
     if /\d+/.match?(token.to_s)
       result = [:integer, token]
     elsif token == :quote
-      value = ''
-      value.concat(get_token(/./)) until get_token(/"/)
-      result = [:string, value]
+      result = parse_string
     elsif [true, false].include?(token)
       result = [:boolean, token]
     elsif token == :lpar
-      result = expression
+      result = parse_expression
       raise Exception, 'Missing ).' unless get_token(/\)/)
     elsif token == :sub
-      result = [:sub, [:integer, '0'], sum]
+      result = [:sub, [:integer, '0'], parse_sum]
     elsif token == :bang
-      result = [:bang, sum]
+      result = [:bang, parse_sum]
     elsif token == :function
-      param, block = function
+      param, block = parse_function
       result = [:function, param, block]
     elsif token
-      result = [:identifier, token]
-      if get_token(/\(/)
-        result = [:call, result]
-        arguments = [:arguments]
-        until get_token(/\)/)
-          arguments.append(expression)
-          get_token(/\s*,\s*/)
-        end
-        result.append(arguments)
-      end
+      result = parse_indentifier(token)
     else
       raise Exception, 'Expression Error.'
     end
     result
   end
 
-  def block
-    token = get_token(/\s*{\s*/)
-    raise Exception, 'Missing {.' if token != :lbrace
+  def parse_string
+    value = ''
+    value.concat(get_token(/./)) until get_token(/"/)
+    [:string, value]
+  end
 
-    result = [:block]
-    skip_new_line
-    until get_token(/\s*}\s*/)
-      result.append(statement)
-      skip_new_line
+  def parse_indentifier(token)
+    result = [:identifier, token]
+    if get_token(/\(/)
+      result = [:call, result]
+      arguments = [:arguments]
+      until get_token(/\)/)
+        arguments.append(parse_expression)
+        get_token(/\s*,\s*/)
+      end
+      result.append(arguments)
     end
-
     result
   end
 
-  def function
+  def parse_function
     token = get_token(/\(/)
     raise Exception 'Missing (.' if token != :lpar
 
@@ -221,26 +212,33 @@ class RootCore
       parameters.append(parameter)
       get_token(/\s*,\s*/)
     end
-    [parameters, block]
+    [parameters, parse_block]
+  end
+
+  def parse_block
+    token = get_token(/\s*{\s*/)
+    raise Exception, 'Missing {.' if token != :lbrace
+
+    result = [:block]
+    skip_new_line
+    until get_token(/\s*}\s*/)
+      result.append(parse_statement)
+      skip_new_line
+    end
+
+    result
   end
 
   def eval(exp, env)
     case exp[0]
     when :statements
-      exp.drop(1).each do |statement|
-        result, is_return = eval(statement, env)
-        return result if is_return
-      end
+      eval_statements(exp, env)
     when :statement
       eval(exp[1], env)
     when :expression
       eval(exp[1], env)
     when :assign
-      raise Exception, 'Identifier contains a reserved word.' if @@keywords.include?(exp[1])
-
-      register_params(exp[1], exp[2]) if exp[2][0] == :function
-      env.set(exp[1], eval(exp[2], env))
-      nil
+      eval_assingment(exp, env)
     when :return
       result = eval(exp[1], env)
       [result, true]
@@ -269,32 +267,11 @@ class RootCore
     when :function
       exp[2]
     when :call
-      name = exp[1][1]
-      args = exp[2].clone.drop(1)
-
-      outer = env
-      env = Environment.new_enclosed(outer)
-      @@params_heap[name].zip(args).each do |param, arg|
-        env.set(param, eval(arg, outer))
-      end
-
-      function_block = eval(exp[1], env)
-      eval(function_block, env)
+      eval_call(exp, env)
     when :if
-      outer = env
-      env = Environment.new_enclosed(outer)
-      if eval(exp[1], outer)
-        eval(exp[2], env)
-      elsif exp[3]
-        eval(exp[3], env)
-      end
+      eval_if(exp, env)
     when :block
-      statements = exp.drop(1)
-      statements.each do |statement|
-        result, is_return = eval(statement, env)
-        return result if is_return
-      end
-      result
+      eval_block(exp, env)
     when :integer
       exp[1].to_i
     when :string
@@ -306,6 +283,54 @@ class RootCore
     end
   end
 
+  def eval_statements(exp, env)
+    exp.drop(1).each do |statement|
+      result, is_return = eval(statement, env)
+      return result if is_return
+    end
+  end
+
+  def eval_assingment(exp, env)
+    raise Exception, 'Identifier contains a reserved word.' if @@keywords.include?(exp[1])
+
+    register_params(exp[1], exp[2]) if exp[2][0] == :function
+    env.set(exp[1], eval(exp[2], env))
+    nil
+  end
+
+  def eval_call(exp, env)
+    name = exp[1][1]
+    args = exp[2].clone.drop(1)
+
+    outer = env
+    env = Environment.new_enclosed(outer)
+    @@params_heap[name].zip(args).each do |param, arg|
+      env.set(param, eval(arg, outer))
+    end
+
+    function_block = eval(exp[1], env)
+    eval(function_block, env)
+  end
+
+  def eval_if(exp, env)
+    outer = env
+    env = Environment.new_enclosed(outer)
+    if eval(exp[1], outer)
+      eval(exp[2], env)
+    elsif exp[3]
+      eval(exp[3], env)
+    end
+  end
+
+  def eval_block(exp, env)
+    statements = exp.drop(1)
+    statements.each do |statement|
+      result, is_return = eval(statement, env)
+      return result if is_return
+    end
+    result
+  end
+
   def register_params(name, function)
     params = function[1].drop(1)
     @@params_heap[name] = params
@@ -314,7 +339,7 @@ class RootCore
   def initialize
     @scanner = StringScanner.new(ARGF.read)
     env = Environment.new
-    eval(parse, env)
+    eval(parse_program, env)
     # p parse
   end
 end
